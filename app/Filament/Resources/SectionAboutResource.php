7177e24aee09faffe06f85a2d903279229afdcd0
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-09-23 13:12:41 +0300

    delet action in about section

--- app/Filament/Resources/SectionAboutResource.php
+++ app/Filament/Resources/SectionAboutResource.php
@@ -100,7 +100,8 @@ class SectionAboutResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-            ])
+                Tables\Actions\DeleteAction::make(),
+                ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
